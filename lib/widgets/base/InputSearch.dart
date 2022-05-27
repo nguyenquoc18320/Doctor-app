34e8ae15d332144b1b81a2faa1df5f9920491fd1
Quynh Hoang <htnquynh@users.noreply.github.com>
2022-05-28 00:07:39 +0700

    fix: home user UI

---
+++ lib/widgets/base/InputSearch.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class InputSearch extends StatelessWidget {
+  String title;
+  final textController;
+
+  InputSearch({
+    Key? key,
+    required this.title,
+    required this.textController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: textController,
+      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Color(0xFFFFFFFF),
+        hintText: title,
+        hintStyle: TextStyle(color: Colors.black38),
+        suffixIcon: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.search),
+        ),
+        border: OutlineInputBorder(
+            borderSide: BorderSide(width: 2.0, color: Colors.transparent),
+            borderRadius: BorderRadius.all(Radius.circular(40))),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 2.0, color: Colors.transparent),
+            borderRadius: BorderRadius.all(Radius.circular(40))),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      ),
+    );
+  }
+}
